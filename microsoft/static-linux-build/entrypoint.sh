1728964094f08cece80101fd58b4839b6c26e676
Ben McMorran <bemcmorr@microsoft.com>
2022-04-12 16:57:07 -0700

    Add --enable-picoprobe flag
    This flag is described in Appendix A of
    https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf.
    
    Change-Id: Iea1ae2d4d6f793be0eea2f98b7fb6d4886243de8

--- microsoft/static-linux-build/entrypoint.sh
+++ microsoft/static-linux-build/entrypoint.sh
@@ -79,7 +79,8 @@ readonly openocd_name="openocd-${openocd_tag}-linux"
 ./bootstrap
 ./configure --build="$(${workspace_dir}/config.guess)" \
     --prefix="${install_dir}/${openocd_name}" \
-    --enable-static --enable-all-static --disable-shared
+    --enable-static --enable-all-static --disable-shared \
+    --enable-picoprobe
 make
 make install-strip
 tar -C "${install_dir}" -czf "${workspace_dir}/${openocd_name}.tar.gz" "${openocd_name}"
